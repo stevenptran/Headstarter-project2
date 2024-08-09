c6b9df06219aedabe9b68ff5c7124dc18fed5a0c
stevenptran <tran.steven.p@gmail.com>
2024-08-08 19:25:22 -0500

    Hidden api keys

--- firebase.js
+++ firebase.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyByjsb1KDbcGhwBHXPX_xxYeoqBW8cU2JU",
-  authDomain: "inventory-management-859df.firebaseapp.com",
-  projectId: "inventory-management-859df",
-  storageBucket: "inventory-management-859df.appspot.com",
-  messagingSenderId: "1059460188578",
-  appId: "1:1059460188578:web:712daf3d36a43146578afb",
-  measurementId: "G-2B1BSED5VQ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
